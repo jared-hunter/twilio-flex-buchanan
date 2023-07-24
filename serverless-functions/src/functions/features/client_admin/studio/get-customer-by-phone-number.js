96740f68a9b60871b64520be301e5795bc66c3d3
Jared Hunter <jhunter@twilio.com>
2023-07-24 08:51:47 -0500

    adding example interface for database

---
+++ serverless-functions/src/functions/features/client_admin/studio/get-customer-by-phone-number.js
@@ -0,0 +1,35 @@
+const { prepareStudioFunction } = require(Runtime.getFunctions()['common/helpers/function-helper'].path);
+
+const requiredParameters = [
+  { key: 'phoneNumber', purpose: 'the number of the inbound call used to lookup the client' },
+];
+
+exports.handler = prepareStudioFunction(requiredParameters, async (context, event, callback, response, handleError) => {
+  try {
+    const { phoneNumber } = event;
+
+    const database = {
+      '+18442832394': {
+        client_name: 'Alcon',
+        itsm: 'Service Now',
+        channel: 'voice', // need to confirm how to lookup clients uniquely for chat
+        language: 'eng',
+        priority: '50',
+      },
+    };
+
+    const result = {};
+
+    try {
+      result.result = database[phoneNumber];
+      result.status = 'success';
+    } catch (error) {
+      result.status = 'not found';
+    }
+
+    response.setBody(result);
+    return callback(null, response);
+  } catch (error) {
+    return handleError(error);
+  }
+});
